7813a1740043a7cf2e0ab7c89ad6c9d9989dd9cb
Nikki Pylman <nikkilea324@gmail.com>
2026-09-20 14:56:39 -0400

    Add campaign hero and turn state

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -11,6 +11,16 @@ var turn: int = 1
 var active_layer: CampaignLayer = CampaignLayer.WORLD
 var active_territory_id: String = ""
 var selected_hex := Vector2i(-1, -1)
+var hero: HeroState
+
+func initialize_hero(hero_id: String, hero_name: String, start_hex: Vector2i, movement: int = 2) -> void:
+    hero = HeroState.new()
+    hero.configure(hero_id, hero_name, start_hex, movement)
+
+func begin_campaign_turn() -> void:
+    turn += 1
+    if hero != null:
+        hero.begin_turn()
 
 func begin_turn() -> void:
     turn += 1
